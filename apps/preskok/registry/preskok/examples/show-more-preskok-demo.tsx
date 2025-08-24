697023c64019496934f9c9ba3730c2b579b8cb7d
GregorGabric <gregor.gabric@outlook.com>
2025-08-24 09:20:46 +0200

    fix

--- apps/preskok/registry/preskok/examples/show-more-preskok-demo.tsx
+++ apps/preskok/registry/preskok/examples/show-more-preskok-demo.tsx
@@ -1,12 +1,26 @@
 "use client"
 
+import { ChevronDownIcon } from "lucide-react"
+import { twMerge } from "tailwind-merge"
+
 import { ShowMore } from "@/registry/preskok/ui/preskok-ui/show-more"
 
 export default function ShowMorePreskokDemo() {
   return (
-    <div className="space-y-3">
-      <ShowMore as="button">Show more</ShowMore>
-      <ShowMore as="text" text="More results" />
+    <div className="py-6">
+      <ShowMore>
+        {({ isSelected }) => (
+          <>
+            Show {isSelected ? "less" : "more"}
+            <ChevronDownIcon
+              className={twMerge(
+                isSelected ? "rotate-180" : "",
+                "size-4 transition-transform duration-200"
+              )}
+            />
+          </>
+        )}
+      </ShowMore>
     </div>
   )
 }
