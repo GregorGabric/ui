d5b590c1a972d3bea5b38397a7842bf17f9280db
GregorGabric <gregor.gabric@outlook.com>
2025-09-04 13:38:28 +0200

    avatar fix

--- apps/preskok/registry/preskok/ui/avatar.tsx
+++ apps/preskok/registry/preskok/ui/avatar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from "react"
+import React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
 import { cn } from "@/lib/utils"
@@ -50,4 +50,4 @@ function AvatarFallback({
   )
 }
 
-export { Avatar, AvatarImage, AvatarFallback }
+export { Avatar, AvatarFallback, AvatarImage }
